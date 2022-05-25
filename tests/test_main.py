67ee95ed4ab60e1db63f6b586aae41ac6a74e624
Bernardo Penteado <bernardopbf3@gmail.com>
2022-05-25 18:19:15 +0100

    Added sequence datasets for testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,20 +2,23 @@ from ukkomotif.main import compute_kmer_frequencies, compute_kmer_conservation_f
 
 import pytest
 
-test_sequence_data = "AAATGGCCGCGCCG--#AAA---TGGC----CGCGCCG#GGCTGTTGAGCGCGCGG-GA#"
-test_conservation_data = "***  *    ** *  #***            ***   #*    ***            #"
+sample_sequence_data = "AAATGGCCGCGCCG--#AAA---TGGC----CGCGCCG#GGCTGTTGAGCGCGCGG-GA#"
+sample_conservation_data = "***  *    ** *  #***            ***   #*    ***            #"
+
+full_sequence_file = "allinter"
+full_conservation_file = "allintercons"
 
 def test_compute_kmer_frequencies():
-    kmer_frequencies = compute_kmer_frequencies(test_sequence_data, 3, False)
+    kmer_frequencies = compute_kmer_frequencies(sample_sequence_data, 3, False)
     assert len(kmer_frequencies) == 32
     assert kmer_frequencies['AAA'] == 2
     with pytest.raises(KeyError):
         assert kmer_frequencies["GAA"]
     with pytest.raises(FileNotFoundError):
-        assert compute_kmer_frequencies(test_sequence_data, 3)
+        assert compute_kmer_frequencies(sample_sequence_data, 3)
 
 def test_compute_kmer_conservation_frequencies():
-    kmer_conservation_frequencies = compute_kmer_conservation_frequencies(test_sequence_data, test_conservation_data, 3, False)
+    kmer_conservation_frequencies = compute_kmer_conservation_frequencies(sample_sequence_data, sample_conservation_data, 3, False)
     assert len(kmer_conservation_frequencies) == 3
     assert kmer_conservation_frequencies["AAA"] == 2
     assert kmer_conservation_frequencies["GCG"] == 1
@@ -24,7 +27,7 @@ def test_compute_kmer_conservation_frequencies():
         assert kmer_conservation_frequencies["GAA"]
 
 def test_compute_kmer_conservations():
-    kmer_conservations = compute_kmer_conservations(test_sequence_data, test_conservation_data, 3, False)
+    kmer_conservations = compute_kmer_conservations(sample_sequence_data, sample_conservation_data, 3, False)
     assert len(kmer_conservations) == 3
     assert kmer_conservations["AAA"] == 1
     assert kmer_conservations["GCG"] == 0.2
